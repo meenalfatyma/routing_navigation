05b3f382b1452931e9f8735c982915d87c31349e
Minal Fatima <meenalfatymah@gmail.com>
2023-04-02 20:30:43 +0500

    04/02/2023 20:30

--- lib/main.dart
+++ lib/main.dart
@@ -22,29 +22,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink.shade200,
-      appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: const Center(child: Text('Click button to get your reward')),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Image(
-                width: 150,
-                height: 150,
-                image: AssetImage("assets/reward.png")),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
-              onPressed: () {
-                Navigator.pushNamed(context, '/details');
-              },
-              child: const Text('Surprise!'),
-            ),
-          ],
+      appBar: null,
+     body: Container(
+    width: double.infinity,
+    height: double.infinity,
+    decoration: const BoxDecoration(
+    image: DecorationImage(
+    image: AssetImage("assets/food2.jpg"),
+    fit: BoxFit.cover,
+    ),
+    ),
+    child: Center(
+      child:  Container(
+        width: 100,
+        height: 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
         ),
-      ),
+        child:ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+        onPressed: () {
+          Navigator.pushNamed(context, '/details');
+        },
+        child: const Text('Menu!',
+          style:TextStyle(color: Colors.black) ,),
+      ),),
+    ), ),
     );
   }
 }
@@ -55,23 +58,406 @@ class DetailScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Center(child:Text('HURRAH!')),
+        backgroundColor: Colors.black,
+        title: const Center(child:Text('Have a Look at Menu')),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/R.gif',
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Get Award Again'),
-            ),
-          ],
+
+      body:
+      Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/food2.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset(
+                        'assets/burger2.png',
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Zinger Burger',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "300Rs.",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height:150,
+
+                      child: Image.asset(
+                        'assets/pizza.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Pizza(Small)',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '400Rs.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+
+                      child: Image.asset(
+                        'assets/pizza.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Pizza(Medium)',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '800Rs.',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset(
+                        'assets/pizza.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Pizza(Large)',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '1200Rs.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+
+                      child: Image.asset(
+                        'assets/pasta.jpg',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Alfredo Pasta',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '500Rs.',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.black
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset(
+                        'assets/fries.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Fries(Small)',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '70Rs.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+
+                      child: Image.asset(
+                        'assets/fries.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Fries(Medium)',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '120Rs.',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),),
+              Container(
+                height: 100,
+                width: 700,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.black
+                ),
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: Image.asset(
+                        'assets/fries.png',
+
+                      ),
+                    ),
+                    // add a gap between the text and text field
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Fries(Large)',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '150Rs.',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+
+                          ),
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
